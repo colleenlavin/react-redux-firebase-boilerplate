52bc649133f252d9ad678e1a461093206b83902e
colleenlavin <cml388@mun.ca>
2017-07-31 16:10:58 -0400

    Added Firebase data

--- src/app/config.js
+++ src/app/config.js
@@ -1,13 +1,13 @@
 module.exports = {
 
-  // Change this to your firebase configuration! (Add Firebase to your web app)
+    // Change this to your firebase configuration! (Add Firebase to your web app)
     FIREBASE_CONFIG: {
 
-        apiKey: 'AIzaSyAAzWIkcMqyHueh1yQt84HdpMFfzL1OZYc',
-        authDomain: 'react-redux-firebase-d6283.firebaseapp.com',
-        databaseURL: 'https://react-redux-firebase-d6283.firebaseio.com',
-        storageBucket: 'react-redux-firebase-d6283.appspot.com',
-        messagingSenderId: '425723634684',
-
+        apiKey: "AIzaSyD6OtytBizCWt3EVlBC6wnFfcqCwogE60w",
+        authDomain: "daisy-73f30.firebaseapp.com",
+        databaseURL: "https://daisy-73f30.firebaseio.com",
+        projectId: "daisy-73f30",
+        storageBucket: "",
+        messagingSenderId: "864278464577"
     },
 };
