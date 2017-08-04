ed168109f3d61f6df3d79978319273718435f634
b3bold <weworkweb@gmail.com>
2017-08-04 06:55:59 -0500

    checklist task page.

--- src/app/components/todo.jsx
+++ src/app/components/todo.jsx
@@ -10,32 +10,165 @@ export default class ToDo extends React.Component {
 
     render() {
         return (
-            <div className='goalsMain'>
-                <div className='goalsMain max-w-800'>
-                    <h2 className="has-text-left is-size-2 thin">Checklist</h2>
-
-                    <ol id="myUL" >
-                        <li className="box">  <Link to={`/info`}> Fill out our form  </Link></li>
-                        <li className="box">  <Link to={`/vendors`}> Explore your options  </Link></li>
-                        <li className="box">  <Link to="http://www.floridahealth.gov/%5C%5C/certificates/certificates/death/index.html"> Order death certificate </Link></li>
-                        <li className="checked" className="box">Choose a date <h6>August 6, 2017</h6></li>
-                        <li className="box">  <Link to={`/`}> Choose your floral arrangements  </Link></li>
-                        <li className="box">  <Link to={`/funeralhomes`}> Arrange your funeral service  </Link></li>
-                        <li className="box">  <Link to={`/reception`}> Pick a reception venue  </Link></li>
-                        <li className="box">  <Link to={`/catering`}> Find your caterer  </Link></li>
-                         <li className="box">  <Link to={`/grimreaper`}> Send your event page to all your guests  </Link></li>
-                        <li className="box">Pick up Aunt Sheila from the airport. <h6>Friday, 2:00 PM</h6></li>
-                    </ol>
-
+            <div className="todo-centered">
+                <h2 className="is-size-2 thin">Checklist</h2>
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">1</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                            <Link to={`/info`}>
+                                <div className="col-md-9 link p-16">
+                                    <h3>Fill out our form </h3>
+                                </div>
+                            </Link> 
+                        </div>
+                    </div>
                 </div>
-                <div >
-                    <form onSubmit={this.addItem}>
-                        <input ref={(a) => this._inputElement = a}
-                            placeholder="Enter additional task">
-                        </input>
-                        <button type="submit" className="addBtn">+</button>
-                    </form>
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">2</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                            <Link to={`/vendors`}>
+                                <div className="col-md-9 link p-16">
+                                    <h3>Explore your options</h3>
+                                </div>
+                            </Link> 
+                        </div>
+                    </div>
                 </div>
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">3</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                            <Link to="http://www.floridahealth.gov/%5C%5C/certificates/certificates/death/index.html">
+                                <div className="col-md-9 link p-16">
+                                    <h3>Order death certificate</h3>
+                                </div>
+                            </Link> 
+                        </div>
+                    </div>
+                </div>
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">4</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                                <div className="col-md-9 link p-16">
+                                    <h3><i className="fa fa-check big-check"></i> Choose a date <span className="bold">August 6, 2017</span></h3>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">4</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                            <Link to={`/`}>
+                                <div className="col-md-9 link p-16">
+                                    <h3>Choose your floral arrangements</h3>
+                                </div>
+                            </Link> 
+                        </div>
+                    </div>
+                </div>
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">5</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                            <Link to={`/funeralhomes`}>
+                                <div className="col-md-9 link p-16">
+                                    <h3>Arrange your funeral service </h3>
+                                </div>
+                            </Link> 
+                        </div>
+                    </div>
+                </div>       
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">6</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                            <Link to={`/reception`}>
+                                <div className="col-md-9 link p-16">
+                                    <h3>Pick a reception venue</h3>
+                                </div>
+                            </Link> 
+                        </div>
+                    </div>
+                </div>  
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">7</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                            <Link to={`/catering`}>
+                                <div className="col-md-9 link p-16">
+                                    <h3>Find your caterer </h3>
+                                </div>
+                            </Link> 
+                        </div>
+                    </div>
+                </div>    
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">8</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                            <Link to={`/grimreaper`}>
+                                <div className="col-md-9 link p-16">
+                                    <h3>Send your event page to all your guests </h3>
+                                </div>
+                            </Link> 
+                        </div>
+                    </div>
+                </div>     
+                <div className="row m-t-16">
+                    <div className="col-md-2">
+                        <h1 className="is-size-2 has-text-centered">9</h1>
+                    </div> 
+                    <div className="col-md-10">
+                        <div className="row shadow todo">
+                            <Link to={`/grimreaper`}>
+                                <div className="col-md-9 link p-16">
+                                    <h3 >Pick up Aunt Sheila from the airport. <span className="bold">Friday, 2:00 PM</span></h3>
+                                </div>
+                            </Link> 
+                        </div>
+                    </div>
+                </div>   
+                <form onSubmit={this.addItem}>
+                    <div className="row m-t-16">
+                        <div className="col-md-2">
+                            
+                        </div> 
+                        <div className="col-md-10">
+                            <div className="row  todo">
+
+                                    <div className="col-md-9 link p-16">
+                                        <input ref={(a) => this._inputElement = a}
+                                placeholder="Enter additional task">
+                            </input><button type="submit" className="addBtn">+</button>
+                                    </div>
+
+                            </div>
+                        </div>
+                    </div>  
+                </form>
+
             </div>
 
         );
